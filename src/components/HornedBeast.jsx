7a695c997d0c33498a875fa3e34a21cacfa5654f
jdabassett <jacobbassett@gmail.com>
2023-02-13 18:33:57 -0700

    small fixes after submission

--- src/components/HornedBeast.jsx
+++ src/components/HornedBeast.jsx
@@ -1,22 +1,18 @@
 import React from "react";
 
 export default class HornedBeast extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // }
-
   render() {
     return (
       <div className="beast-container">
-        <h2 className='beast-title'>{this.props.title}</h2>
+        <h2 className="beast-title">{this.props.title}</h2>
         <img
-          className='beast-image'
+          className="beast-image"
           src={this.props.imageUrl}
           alt={this.props.description}
           title={this.props.title}
           width="200px"
         />
-        <p className='beast-description'>{this.props.description}</p>
+        <p className="beast-description">{this.props.description}</p>
       </div>
     );
   }
